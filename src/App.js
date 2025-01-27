774a429e93e818362b883f811814cc8bbc0a323e
Isaiah Nields <zaynields@gmail.com>
2025-01-27 13:13:13 -0800

    Add verses

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./App.css";
-import { CHAPTER_53_VERSE_5 } from "./Isaiah";
+import {
+  CHAPTER_50_VERSE_6,
+  CHAPTER_53_VERSE_5,
+  CHAPTER_53_VERSE_8,
+  CHAPTER_53_VERSE_12,
+  CHAPTER_56_VERSE_4,
+} from "./Isaiah";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div className="pulse">{CHAPTER_50_VERSE_6}</div>
         <div className="pulse">{CHAPTER_53_VERSE_5}</div>
+        <div className="pulse">{CHAPTER_53_VERSE_8}</div>
+        <div className="pulse">{CHAPTER_53_VERSE_12}</div>
+        <div className="pulse">{CHAPTER_56_VERSE_4}</div>
       </header>
     </div>
   );
