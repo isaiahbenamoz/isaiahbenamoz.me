8cef70f93e411ac1faf97e2088d210b99f621b76
Isaiah Nields <zaynields@gmail.com>
2025-01-27 12:44:32 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import { CHAPTER_53_VERSE_5 } from "./Isaiah";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="pulse">{CHAPTER_53_VERSE_5}</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
