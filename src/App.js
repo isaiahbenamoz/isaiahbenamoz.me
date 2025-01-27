68660fab549df9986f6ae38c2da1d201297cc181
Isaiah Nields <zaynields@gmail.com>
2025-01-27 13:18:48 -0800

    use loop to iterate through verses

--- src/App.js
+++ src/App.js
@@ -7,15 +7,22 @@ import {
   CHAPTER_56_VERSE_4,
 } from "./Isaiah";
 
+const VERSES = [
+  CHAPTER_50_VERSE_6,
+  CHAPTER_53_VERSE_5,
+  CHAPTER_53_VERSE_8,
+  CHAPTER_53_VERSE_12,
+  CHAPTER_56_VERSE_4,
+];
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className="pulse">{CHAPTER_50_VERSE_6}</div>
-        <div className="pulse">{CHAPTER_53_VERSE_5}</div>
-        <div className="pulse">{CHAPTER_53_VERSE_8}</div>
-        <div className="pulse">{CHAPTER_53_VERSE_12}</div>
-        <div className="pulse">{CHAPTER_56_VERSE_4}</div>
+        {Array.from({ length: 100 }).map(() => {
+          const randomIndex = Math.floor(Math.random() * VERSES.length);
+          return <div className="pulse">{VERSES[randomIndex]}</div>;
+        })}
       </header>
     </div>
   );
